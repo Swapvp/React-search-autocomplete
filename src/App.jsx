169c1e419a1244329788dd8ce913977870e32e95
Swapvp <swapneshpawar1994@gmail.com>
2024-04-28 10:15:53 +0530

    search autocomplete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SearchAutocomplete from "./Component/SearchAutocomplete";
+
+function App() {
+  return (
+    <>
+      <SearchAutocomplete />
+    </>
+  );
+}
+
+export default App;
